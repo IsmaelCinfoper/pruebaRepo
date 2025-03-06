381501d4613e0befff487a0b3850c96115633470
ismael <ismaelcinfoper@gmail.com>
2025-03-06 18:34:36 +0100

    De nuevo modificacion en ram3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Documentation Este es mi primer cambio</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
-                                                AHORA ESTE ES EL CAMBIO PARA EL OTRO \ SEGUIMOS TRABAJANDO/ AHORA ES CUANDO SE LIA GORDA Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                                AHORA ESTE ES EL CAMBIO PARA EL OTRO \ SEGUIMOS TRABAJANDO/ AHORA ES CUANDO SE LIA GORDA *** Continuanmo liandola en la RAM3 esto no para Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                             </p>
                                         </div>
                                     </div>
